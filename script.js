2096d424973d7bb6c7da985493242a987c53dcfc
ABHISHEK BIRADAR <162307955+seekernothing@users.noreply.github.com>
2025-12-25 09:59:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function debounce(callback, delay, immediate = false) {
-  
+
+	let timer
+	return function(...args){
+		let context = this
+		if(timer){
+			clearTimeout(timer)
+		}
+
+		timer = setTimeout(()=>{
+			callback.apply(args,context)
+		},delay)
+	}
   }
   
   module.exports = debounce;
